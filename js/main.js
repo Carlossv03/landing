968a20a31239550db3de4471cc0d59fb559a46f8
Carlossv03 <carasala@espol.edu.ec>
2024-10-28 11:32:47 -0500

    Guia08 completada

--- js/main.js
+++ js/main.js
@@ -1,31 +1,72 @@
+const databaseURL = 'https://landingcasv-default-rtdb.firebaseio.com/colection.json'; 
+
+ let sendData = () => { 
+    
+    // Obtén los datos del formulario
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries()); // Convierte FormData a objeto
+
+    // new Date().toLocaleString( locales, options )
+    data['saved'] = new Date().toLocaleString('es-CO', { timeZone: 'America/Guayaquil' })
+
+     // Realiza la petición POST con fetch
+     fetch(databaseURL, {
+        method: 'POST', // Método de la solicitud
+        headers: {
+            'Content-Type': 'application/json' // Especifica que los datos están en formato JSON
+        },
+        body: JSON.stringify(data) // Convierte los datos a JSON
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Error en la solicitud: ${response.statusText}`);
+        }
+        return response.json(); // Procesa la respuesta como JSON
+    })
+    .then(result => {
+        alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces'); // Maneja la respuesta con un mensaje
+        form.reset()
+    })
+    .catch(error => {
+        alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+    });
+
+
+
+  }
+
+
+
+
+
+
 let ready = () => {
     console.log('Dom está listo')
 }
 let loaded = () => {
-    var myform = document.getElementById('form')
+    let myform = document.getElementById('form')
     myform.addEventListener("submit", (eventSubmit) =>{eventSubmit.preventDefault();
 
     var emailElement = document.querySelector('.form-control-lg')
     var emailText = emailElement.value;
-    if (emailText.length == 0){
+    if (emailText.length === 0) {
+        emailElement.animate(
+            [
+                { transform: "translateX(0)" },
+                { transform: "translateX(50px)" },
+                { transform: "translateX(-50px)" },
+                { transform: "translateX(0)" }
+            ],
+            {
+                duration: 400,
+                easing: "linear",
+            }
+        )
         emailElement.focus()
+        return;
     }
-    emailElement.animate(
-        [
-            { transform: "translateX(0)" },
-            { transform: "translateX(50px)" },
-            { transform: "translateX(-50px)" },
-            { transform: "translateX(0)" }
-        ],
-        {
-            duration: 400,
-            easing: "linear",
-        }
-    )
-
-    }
-    
-)
+    sendData();
+})
     
 }
 
